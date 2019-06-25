8bffc496b1dc6eeca228ff0f5a08e006e38c0f5b
elena-13 <goryainova.alena@gmail.com>
2019-06-25 17:03:11 +0300

    :repeat:removing legacy code

--- client/src/components/calendar/calendarParams.js
+++
@@ -1,5 +0,0 @@
-export const getCalendarParams = triggerName => ({
-    trigger: triggerName,
-    monthArray: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-    format: 'MM/yyyy',
-});
