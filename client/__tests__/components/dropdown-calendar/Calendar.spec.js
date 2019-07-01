e7e548396e54f63878bee3fb6f24172553b5dc49
elena-13 <goryainova.alena@gmail.com>
2019-07-01 11:29:20 +0300

    :repeat:change Calendar's  tests

--- client/__tests__/components/dropdown-calendar/Calendar.spec.js
+++ client/__tests__/components/dropdown-calendar/Calendar.spec.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import DatePicker from 'react-datepicker';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Calendar from '../../../src/components/calendar/Calendar';
 import CalendarButton from '../../../src/components/calendar/CalendarButton';
@@ -13,13 +13,13 @@ describe('Calendar component', () => {
   };
 
   it('should render Calendar component', () => {
-    const component = mount(<Calendar {...props} />);
+    const component = shallow(<Calendar {...props} />);
 
     expect(component).toBeTruthy();
   });
 
   it('should render DatePicker component', () => {
-    const component = mount(<Calendar {...props} />);
+    const component = shallow(<Calendar {...props} />);
 
     expect(component.find(DatePicker).length).toEqual(1);
   });
